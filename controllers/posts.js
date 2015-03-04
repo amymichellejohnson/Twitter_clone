b3364db75226740e339eb2162f52bb3e340e2c2f
Lauren Marcus <lauren.e.marcus@gmail.com>
2015-03-03 17:34:15 -0800

    Add ability to add comments to each tweet

--- controllers/posts.js
+++ controllers/posts.js
@@ -4,7 +4,7 @@ Twitter.PostsController = Ember.ArrayController.extend ({
   sortProperties: ['date'],
   actions: {
     addTweet: function() {
-      tweets.addObject({id: tweets.length + 1, username: this.username, post: this.post, date: new Date().toLocaleString()});
+      tweets.addObject({id: tweets.length + 1, username: this.username, post: this.post, date: new Date().toLocaleString(), comments: []});
     },
 
   }
@@ -19,6 +19,16 @@ Twitter.DateController = Ember.ObjectController.extend ({
       } else {
         this.set('isDateShowing', true);
       }
+    },
+    addComment: function(comment) {
+      console.log(comment)
+      var tweetComments = this.get('model.comments');
+      // temp = this.get('comments');
+      tweetComments.addObject(comment);
+      // console.log(temp)
+      // this.set('comments', temp);
+      console.log(tweetComments)
+
     }
   }
 });
--- store.js
+++ store.js
@@ -3,18 +3,21 @@ var tweets = [
     id: '1',
     username: 'Zaki Qayoumi',
     post: 'I can has cheeseburger',
-    date: '1/25/15, 12:27:08 PM'
+    date: '1/25/15, 12:27:08 PM',
+    comments: ['hello', 'have a great day!']
   },
   {
     id: '2',
     username: 'Bob Loblaw',
     post: 'Read Bob Loblaw\'s Law Blog!',
-    date: '4/17/15, 12:27:08 PM'
+    date: '4/17/15, 12:27:08 PM',
+    comments: ['howdy']
   },
   {
     id: '3',
     username: 'Dirk Diggler',
     post: 'I am a big, bright, shining star',
-    date: '1/25/15, 12:30:08 PM'
+    date: '1/25/15, 12:30:08 PM',
+    comments: ['yo']
   }
 ]
